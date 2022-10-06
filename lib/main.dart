01db6549786cd78b80e69ea874598676e23ee343
chandandas9353r <chandandas9353r@gmail.com>
2022-10-06 22:51:59 +0530

    Background set, pubspec.yaml updated

--- lib/main.dart
+++ lib/main.dart
@@ -20,58 +20,66 @@ class _WelcomeScreenState extends State<WelcomeScreen>{
   Widget build(BuildContext context){
   final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Colors.orange,
-      body: Center(
-        child: ListView(
-          itemExtent: screenHeight*0.75,
-          padding: const EdgeInsets.all(20),
-          physics: const NeverScrollableScrollPhysics(),
-          children: [
-            SizedBox(
-                  height: screenHeight/2,
-                  child: 
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            repeat: ImageRepeat.repeat,
+            fit: BoxFit.fill,
+            image: AssetImage("lib/assets/images/background.jpeg"),
+          ),
+        ),
+        child: Center(
+          child: ListView(
+            itemExtent: screenHeight*0.75,
+            padding: const EdgeInsets.all(20),
+            physics: const NeverScrollableScrollPhysics(),
+            children: [
+              SizedBox(
+                    height: screenHeight/2,
+                    child: 
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                          ),
+                          child: const Icon(Icons.android,size: 100,),
                         ),
-                        child: const Icon(Icons.android,size: 100,),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      InkWell(
+                        onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const student_signin.SignIn())),
+                        mouseCursor: SystemMouseCursors.click,
+                        child: Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                          ),
+                          child: const Text("Student",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w700,),textScaleFactor: 2,),
+                        ),
+                      ),
+                      const SizedBox(height: 10,),
+                      InkWell(
+                        onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const faculty_signin.SignIn())),
+                        mouseCursor: SystemMouseCursors.click,
+                        child: Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                          ),
+                          child: const Text("Faculty",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w700,),textScaleFactor: 2.2,),
+                        ),
                       ),
                     ],
                   ),
-                ),
-                Column(
-                  children: [
-                    InkWell(
-                      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const student_signin.SignIn())),
-                      mouseCursor: SystemMouseCursors.click,
-                      child: Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                        ),
-                        child: const Text("Student",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w700,),textScaleFactor: 2,),
-                      ),
-                    ),
-                    const SizedBox(height: 10,),
-                    InkWell(
-                      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const faculty_signin.SignIn())),
-                      mouseCursor: SystemMouseCursors.click,
-                      child: Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                        ),
-                        child: const Text("Faculty",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w700,),textScaleFactor: 2.2,),
-                      ),
-                    ),
-                  ],
-                ),
-          ],
+            ],
+          ),
         ),
       ),
     );
